17a7357ba6e7987b1a7742ad5ba93dfbacfba3a3
skinsler <simonkinsler@gmail.com>
2019-05-06 22:28:46 -0500

    gifs animate when clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
 
     for (let i=0;i<topics.length;i++) {
         let button = $("<button>");
-        button.addClass("topic-btn");
+        button.addClass("btn btn-primary topic-btn");
         button.attr("data-name", topics[i]);
         button.text(topics[i]);
         $("#buttons").append(button);
@@ -12,9 +12,9 @@ $( document ).ready(function() {
 
 
     $(document).on("click", ".topic-btn", function(){
-        var person = $(this).attr("data-name");
+        var topic = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-          person + "&api_key=IaQJ9HSoYke3qaiDntRKVdWxOLor3XLC&limit=10&rating=g";
+          topic + "&api_key=IaQJ9HSoYke3qaiDntRKVdWxOLor3XLC&limit=10&rating=g";
     
         $.ajax({
           url: queryURL,
@@ -22,11 +22,49 @@ $( document ).ready(function() {
         })
           .then(function(response) {
             console.log(response);
+            var results = response.data;
+    
+            for (var i = 0; i < results.length; i++) {
+              var gifDiv = $("<div>");
+    
+              var rating = results[i].rating;
+    
+              var p = $("<p>").text("Rating: " + rating);
 
+              var stillImageUrl = results[i].images.fixed_height_still.url;
+              var animatedImageUrl = results[i].images.fixed_height.url;
+    
+              var stillImage = $("<img>");
+              stillImage.attr("src", stillImageUrl);
+              stillImage.attr("data-still", stillImageUrl);
+              stillImage.attr("data-animate", animatedImageUrl);
+              stillImage.attr("data-state", "still");
+              var animatedImage = (results[i].images.fixed_height.url);
+    
+              gifDiv.prepend(p);
+              gifDiv.prepend(stillImage);
+    
+              $("#gifs").prepend(gifDiv);
             }
           });
       });
-        
+    
+    $(document).on("click", "img", function(){  
+  
+        let animatedUrl = $(this).attr("data-animate");
+        let stillUrl = $(this).attr("data-still");
+        let state = $(this).attr("data-state");
+           
+        if ( state === "still") {
+          $(this).attr("src", animatedUrl);
+          $(this).attr("data-state", "animating");
+        }
+        else {
+          $(this).attr("src", stillUrl);
+          $(this).attr("data-state", "still");  
+        };
+
+    });
 
 
 
